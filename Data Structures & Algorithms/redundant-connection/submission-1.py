55c50ee0abdcabaf8a4633d2d12ac075c2d72277
gsina97 <76462227+gsina97@users.noreply.github.com>
2026-09-02 21:36:45 -0700

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.py
@@ -0,0 +1,31 @@
+class UF:
+    def __init__(self,n):
+        self.parent = list(range(n))
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    
+    def union(self, a,b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+
+        if root_a == root_b:
+            return False
+        
+        self.parent[root_b] = root_a
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        n = len(edges)
+
+        uf = UF(n + 1)
+
+        for a,b in edges:
+            if not uf.union(a,b):
+                return [a,b]
+        
