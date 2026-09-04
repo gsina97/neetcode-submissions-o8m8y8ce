fa81922a42b0756f6613b0a5e5b5c89de5e4afb7
gsina97 <76462227+gsina97@users.noreply.github.com>
2026-09-03 19:04:52 -0700

    Add: valid-tree - submission-8

---
+++ Data Structures & Algorithms/valid-tree/submission-8.py
@@ -0,0 +1,33 @@
+class UF:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    
+    def union(self, a, b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+
+        if root_a == root_b:
+            return False
+        
+        self.parent[root_a] = root_b
+        return True
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n -1:
+            return False
+        uf = UF(n)
+
+        for a, b in edges:
+            if not uf.union(a,b):
+                return False
+        
+
+        return True
