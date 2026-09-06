be45e261addd41496411b997a10570b5f27e1531
gsina97 <76462227+gsina97@users.noreply.github.com>
2026-09-05 21:46:52 -0700

    Add: word-break - submission-15

---
+++ Data Structures & Algorithms/word-break/submission-15.py
@@ -0,0 +1,21 @@
+from functools import cache
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        
+        
+        @cache
+        def dfs(i):
+            if i == len(s):
+                return True
+
+            
+
+            for w in wordDict:
+                if s[i: i + len(w)] == w:
+                    if dfs(i + len(w)):
+                        return True
+            return False
+        
+        return dfs(0)
+            
