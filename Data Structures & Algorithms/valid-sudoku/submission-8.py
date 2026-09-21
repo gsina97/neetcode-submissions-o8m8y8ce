7aee29da5276494e0984ffd2e3df524757b0cd1c
gsina97 <76462227+gsina97@users.noreply.github.com>
2026-09-21 13:37:37 -0700

    Add: valid-sudoku - submission-8

---
+++ Data Structures & Algorithms/valid-sudoku/submission-8.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        rows = defaultdict(list)
+        cols = defaultdict(list)
+        square = defaultdict(list)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                
+                if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in square[(i//3, j // 3)]:
+                    return False
+                
+                rows[i].append(board[i][j])
+                cols[j].append(board[i][j])
+                square[(i//3, j // 3)].append(board[i][j])
+        
+        return True
+        
+        
